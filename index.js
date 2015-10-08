163798ced4253194af9229a49cab33da82c91e91
Ildar Sagdejev <specious@gmail.com>
2015-10-08 03:05:43 -0400

    Implemented ability to expand a bitlink or shorten a URL

--- index.js
+++ index.js
@@ -2,35 +2,92 @@
 
 'use strict'
 
-const APP_NAME = 'bitly-client'
-const BITLY_MAX_HISTORY_CHUNK = 100
+const APP_NAME = 'bitly-client',
+      BITLY_MAX_HISTORY_CHUNK = 100
 
 var Bitly = require('bitly'),
     app = require('commander'),
     readline = require('readline-sync'),
     rc = require('rc')(APP_NAME),
+    url = require('url'),
     print = console.log.bind( console )
 
 require('colors')
+
+var domains = {
+  default: [
+    'bit.ly',
+    'bitly.com',
+    'j.mp'],
+  extended: [
+    'www.j.mp',
+    'fb.me',
+    'on.fb.me',
+    'amzn.to',
+    'urls.im',
+    'aol.it',
+    'atmlb.com',
+    'bbc.in',
+    'bhpho.to',
+    'binged.it',
+    'bloom.bg',
+    'buff.ly',
+    'cnet.co',
+    'huff.to',
+    'lat.ms',
+    'nyr.kr',
+    'nyti.ms',
+    'on.fb.me',
+    'on.mtv.com',
+    'on.vh1.com',
+    'oreil.ly',
+    'politi.co',
+    'tcrn.ch',
+    'usat.ly',
+    'wapo.st',
+    'yhoo.it'
+  ]
+}
 
 app
   .description( 'Access Bitly from the command line' )
   .option( '-v, --verbose', 'verbose output' )
   .option( '-c, --count <n>', 'limit results', parseCount, Infinity )
   .option( '--ask', 'ask for Bitly access token' )
+  .option( '--domain [value]', 'preferred Bitly domain for shortening: ' + domains.default.join(', ') )
+  .arguments( '[arg]' )
   .parse( process.argv )
 
-var key = rc.key
+var key = rc.key, action = history
 
-if( rc.ask || !preValidateToken( key ) ) {
-  key = getBitlyToken()
+let arg = app.args[0]
 
-  if( !preValidateToken( key ) )
-    print( "Please check your token, it doesn't look valid.".red )
+if( arg ) {
+  if( checkURI( arg ) ) {
+    let u = url.parse( arg )
+
+    // If the URL is a Bitly URL, then make a request to expand it
+    if( domains.default.indexOf( u.hostname ) !== -1 || domains.extended.indexOf( u.hostname ) !== -1 ) {
+        action = function() { expand( arg ) }
+    } else {
+      action = function() { shorten( arg, app.domain ) }
+    }
+  } else
+    action = function() { expand( arg ) }
 }
 
+if( app.ask || !preValidateToken( key ) )
+  key = askBitlyToken()
+
 var bitly = new Bitly( key )
-getLinkHistory()
+
+action()
+
+function checkURI( s ) {
+  // URI validator by Diego Perini:
+  //   https://gist.github.com/dperini/729294
+  return /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test( s )
+}
 
 function parseCount( n ) {
   n = parseInt( n )
@@ -43,8 +100,8 @@ function preValidateToken( token ) {
   return typeof token === 'string' && token.length !== 0 && ( /^[0-9a-f]+$/ ).test( token )
 }
 
-function getBitlyToken() {
-  print( "Please enter your Bitly OAuth access token." )
+function askBitlyToken() {
+  print( "Please enter your Bitly access token." )
   print( "You can obtain your token from: " + "https://bitly.com/a/oauth_apps".yellow )
   print()
 
@@ -59,7 +116,28 @@ function getBitlyToken() {
   return key
 }
 
-function getLinkHistory( offset ) {
+function printError( error ) {
+  print( (error.toString() + " (code: " + error.code + ")").red )
+}
+
+function expand( shortUrl ) {
+  bitly.expand( shortUrl ).then( res => {
+    let ret = res.data.expand[0]
+    print( (ret.short_url ? ret.short_url.yellow + " > " : "") + ret.long_url.yellow )
+  } ).catch( e => {
+    printError( e )
+  } )
+}
+
+function shorten( longUrl, preferredDomain ) {
+  bitly.shorten( longUrl, preferredDomain ).then( res => {
+    print( res.data.url.yellow )
+  } ).catch( e => {
+    printError( e )
+  } )
+}
+
+function history( offset ) {
   offset = offset || 0
   let count = Math.min( app.count, BITLY_MAX_HISTORY_CHUNK )
 
@@ -67,17 +145,17 @@ function getLinkHistory( offset ) {
     app.count -= count
 
     bitly._doRequest( bitly._generateNiceUrl( { offset, limit: count }, 'user/link_history' ) ).then( res => {
-      printLinkHistory( res.data.link_history )
+      printHistory( res.data.link_history )
 
       if( offset + count < res.data.result_count )
-        getLinkHistory( offset + count )
-    } ).catch( err => {
-      print( (err.toString() + " (code: " + err.code + ")").red )
+        history( offset + count )
+    } ).catch( e => {
+      printError( e )
     } )
   }
 }
 
-function printLinkHistory( link_history ) {
+function printHistory( link_history ) {
   for( let item of link_history ) {
     //
     // Print short link and long URL
