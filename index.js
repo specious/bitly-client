specious/bitly-client

commit 2666c17316b139c8ce2c06b271a9495a685134ee

Take --domain setting into account when displaying bitlink during expansion

index.js
@@ -257,8 +257,14 @@ function expand( shortUrl ) {
         print( shortUrl.grey + " is not yet a bitlink" )
       else
         warn( "error trying to expand " + shortUrl + ": " + ret.error )
-    } else
-      print( (ret.short_url ? makeHttps( ret.short_url ) : "https://bit.ly/" + ret.hash ).yellow + " > " + ret.long_url.yellow )
+    } else {
+      print(
+        ( ret.short_url
+          ? makeHttps( ret.short_url )
+          : "https://" + app.optsObj.domain + "/" + ret.hash
+        ).yellow + " > " + ret.long_url.yellow
+      )
+    }
   } ).catch( e => {
     warn( e )
   } )

commit 5981152b855e86479694267982d9a0727b896ec7

Fixed a bug introduced in 2666c17 that rendered https://undefined/ short links on expansion

index.js
@@ -94,6 +94,8 @@ app
 
 applyAltCount( app.rawArgs )
 
+app.optsObj.domain = app.optsObj.domain || 'bit.ly'
+
 var bitly,
     action = history,
     arg0 = app.args[0]