91b428a5176bf5d8eb1a8e808d637132877e93ca
Ildar Sagdejev <isagdejev@protonmail.com>
2021-11-10 00:43:39 -0500

    Works with Bitly API v4

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # bitly-client
 
-[![Build Status](https://travis-ci.org/specious/bitly-client.svg?branch=master)](https://travis-ci.org/specious/bitly-client)
-
-Use [Bitly](https://bitly.com/) from the command line ([What is Bitly?](http://support.bitly.com/knowledgebase/articles/77224-what-is-bitly))
+Use [Bitly](https://bitly.com/) from the command line
 
 ![bitly-client -c 12](http://specious.github.io/bitly-client/screenshots/bitly-client-1.1.5.png "bitly-client@1.1.5")
 
 ## Install
 
 ```
-npm install -g bitly-client
+pnpm i -g bitly-client
 ```
 
 ## Usage
@@ -20,61 +18,60 @@ bitly-client --help
 
 ## Usage examples
 
-Retrieve all saved bitlinks (in reverse chronological order):
+#### Retrieve all saved bitlinks (in reverse chronological order)
 
 ```
 bitly-client
 ```
 
-Retrieved only the five newest bitlinks:
+#### Retrieve your five most recently created bitlinks
 
 ```
-bitly-client -c 5
+bitly-client -5
 ```
 
-Shorten a long URL:
+#### Shorten a URL
 
 ```
 bitly-client https://www.quora.com/What-are-the-most-interesting-HTML-JS-DOM-CSS-hacks-that-most-web-developers-dont-know-about
 ```
 
-Look up the long url for a bitlink:
+#### Look up the original url that a bitlink redirects to
 
 ```
-bitly-client http://bit.ly/1WOyimn
+bitly-client https://bit.ly/www-net
 ```
 
-Archive a bitlink ([what does that mean?](http://support.bitly.com/knowledgebase/articles/154192-how-do-i-delete-a-bitlink)):
+```
+bitly-client j.mp/www-net
+```
+
+```
+bitly-client www-net
+```
+
+#### Archive a bitlink ([what does that mean?](http://support.bitly.com/knowledgebase/articles/154192-how-do-i-delete-a-bitlink))
 
 ```
 bitly-client -a http://bit.ly/1WOyimn
 ```
 
-Pass multiple arguments:
+#### Pass multiple arguments
 
 ```
-bitly-client regexplained google-crash facts-about-money
+bitly-client regexplained google-crash facts-about-money joebiden-archive
 ```
 
-Use a different short domain (including a [custom domain](https://support.bitly.com/hc/articles/230558107-What-is-a-Branded-Short-Domain-bsd-and-how-do-I-get-one-) added to your Bitly account):
+#### Use an alternative short domain (including a [custom domain](https://support.bitly.com/hc/articles/230558107-What-is-a-Branded-Short-Domain-bsd-and-how-do-I-get-one-))
 
 ```
-bitly-client --domain j.mp https://www.bitballoon.com/blog/2014/10/03/five-reasons-you-want-https-for-your-static-site
+bitly-client --domain j.mp https://github.com/opsmatters/bitly-java-api/tree/1319b3e/src/main/java/com/opsmatters/bitly/api/model/v4 
 ```
 
-Have some fun:
+#### Have some fun (see what existing short links redirect to)
 
 ```
 bitly-client have you ever had a dream that you were so sure was real
-```
-
-## Develop
-
-Clone this repository, then run:
-
-```
-yarn install
-./index.js
 ```
 
 ## License
--- index.js
+++ index.js
@@ -2,18 +2,21 @@
 
 'use strict'
 
-var manifest = require('./package.json'),
-    { BitlyClient } = require('bitly'),
-    app = require('commander'),
-    rc = require('rc')(manifest.name),
-    homedir = require('os').homedir(),
-    url = require('url'),
-    read = require('read'),
-    print = console.log.bind( console )
+const manifest = require('./package.json')
+const app = require('commander')
+const rc = require('rc')(manifest.name)
+const homedir = require('os').homedir()
+const readline = require('readline')
+const { promisify }= require('util')
+const url = require('url')
+const print = console.log.bind(console)
+const c = require('ansi-colors')
 
-require('colors')
+const fetch = (...args) => import('node-fetch').then(
+  ({default: fetch}) => fetch(...args)
+)
 
-const BITLY_MAX_HISTORY_CHUNK = 100
+const indent = (...args) => { console.log("  ", ...args) }
 
 var domains = {
   default: [
@@ -26,6 +29,7 @@ var domains = {
     'bitly.is',
     'on.fb.me',
     'amzn.to',
+    'ebay.to',
     'apple.co',
     'urls.im',
     'aol.it',
@@ -60,6 +64,7 @@ var domains = {
     '4sq.com',
     'abcn.ws',
     'wapo.st',
+    'abt.cm',
     'lat.ms',
     'ind.pn',
     'es.pn',
@@ -67,11 +72,13 @@ var domains = {
     'engt.co',
     'bzfd.it',
     'shebpr.es',
+    'sforce.co',
     'pj.pizza',
     'b-gat.es',
     'm-gat.es',
     'theatln.tc',
     'go.nasa.gov',
+    '1.usa.gov',
     'red.ht',
     'wef.ch',
     'vz.to'
@@ -79,290 +86,352 @@ var domains = {
 }
 
 app
-  .version( manifest.version )
-  .description( 'Access Bitly from the command line' )
-  .option( '-v, --verbose', 'verbose output' )
-  .option( '-c, --count <n>', 'limit results', parseCount, Infinity )
-  .option( '-<n>', 'limit results (same as --count <n>)' )
-  .option( '-a, --archive', 'archive the following bitlink' )
-  .option( '--key <key>', 'provide a Bitly access token', String )
-  .option( '--ask', 'ask for Bitly access token (overrides --key)' )
-  .option( '--save', 'save Bitly access token (use with --key or --ask)' )
-  .option( '--domain <value>', 'preferred Bitly domain for shortening: ' + domains.default.join(', ') )
-  .arguments( '[args]' )
+  .version(manifest.version)
+  .description('Use Bitly from the command line')
+  .option('-c, --count <n>', 'limit results', parseCount, Infinity)
+  .option('-<n>', 'limit results (same as --count <n>)')
+  .option('-a, --archive', 'archive the following bitlink')
+  .option('--key <key>', 'provide a Bitly access token', String)
+  .option('--ask', 'ask for Bitly access token (overrides --key)')
+  .option('--save', 'save Bitly access token (use with --key or --ask)')
+  .option('--domain <value>', 'preferred Bitly domain for shortening: ' + domains.default.join(', '))
+  .option('-v, --verbose', 'verbose output')
+  .option('--raw', 'raw output')
+  .arguments('[args...]')
   .allowUnknownOption()
-  .parse( process.argv )
+  .parse(process.argv)
 
-applyAltCount( app.rawArgs )
-
-var bitly,
+let accessToken,
     action = history,
-    arg0 = app.args[0]
+    opts = app.opts(),
+    args = findCountValue(app.args)
 
-if ( app.archive ) {
-  if ( arg0 ) {
+//
+// Which action are we taking
+//
+if (opts.archive) {
+  if (args[0]) {
     action = archive
   } else {
-    process.exit( 0 )
+    process.exit(0)
   }
-} else if ( arg0 ) {
+} else if (args[0]) {
   action = expandOrShorten
 }
 
-getBitlyToken().then( key => {
-  bitly = new BitlyClient( key )
+;(async () => {
+  try {
+    accessToken = await getAccessToken()
+  } catch (e) { abort(e) }
 
-  if ( arg0 ) {
-    for( let i = 0; i < app.args.length; i++ )
-      action( app.args[i] )
-  } else {
-    action()
-  }
-}, ( e ) => abort( e ) )
+  try {
+    if (args[0]) {
+      for (let i = 0; i < args.length; i++)
+        await action(args[i])
+    } else {
+      await action()
+    }
+  } catch(e) { error(e) }
+})()
 
-function warn( e ) {
-  if ( e ) {
-    if ( app.verbose )
-      print( e.stack )
-    else
-      print( e.message + " " + ("(" + e.name + ")").red )
-  }
+function abort(msg) {
+  print(c.red(msg))
+  process.exit(1)
 }
 
-function abort( e ) {
-  warn( e )
-  process.exit( 1 )
+function error(code) {
+  abort('Bitly service returned: ' + code)
 }
 
-function isQualifiedURI( s ) {
-  // Small but effective URI validator by Diego Perini:
-  //   https://gist.github.com/dperini/729294
-  return /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test( s )
-}
+function parseCount(n) {
+  n = parseInt(n)
 
-function validateURI( s ) {
-  if ( isQualifiedURI( s ) ) {
-    return s
-  } else {
-    s = 'http://' + s
-
-    if ( isQualifiedURI( s ) )
-      return s
-    else
-      return null
-  }
-}
-
-function parseCount( n ) {
-  n = parseInt( n )
-
-  // Clamp input to range [0..Infinity]
+  // Constrain count argument to non-negative values
   return isNaN(n) ? Infinity : (n > 0 ? n : 0)
 }
 
 //
 // Let last occurring -<n> arg override any passed --count
 //
-function applyAltCount( args ) {
-  for( let i = args.length - 1; i >= 0; i-- ) {
-    let arg = args[i], rest = arg.slice( 1 )
+function findCountValue(args) {
+  for (let i = args.length - 1; i >= 0; i--) {
+    let arg = args[i], rest = arg.slice(1)
 
-    if ( arg[0] === "-" && /^\d+$/.test( rest ) ) {
-      app.count = parseInt( rest )
-      break
+    if (arg[0] === "-" && /^\d+$/.test(rest)) {
+      opts.count = parseInt(rest)
+      args.splice(i, 1)
+      return args
     }
+  }
+
+  return args
+}
+
+async function getAccessToken() {
+  if (opts.ask || (!opts.key && !tokenLooksRealistic(rc.key))) {
+    print("Please enter your Bitly access token.")
+    print("Get an API access token here:", c.yellow("https://app.bitly.com/settings/api/"))
+    print()
+
+    let prompt = readline.createInterface({ input: process.stdin, output: process.stdout })
+
+    prompt.question[promisify.custom] = (question) => {
+      return new Promise(
+        resolve => prompt.question(question, resolve)
+      )
+    }
+
+    let key = (await promisify(prompt.question)("Access token: ")).trim()
+    prompt.close()
+    print()
+
+    if (tokenLooksRealistic(key)) {
+      // Save the key if there isn't one saved already or if --save option is passed
+      if (opts.save || !rc.key)
+        saveConfig(key)
+
+      return key
+    } else {
+      throw "Invalid access token"
+    }
+  } else if (opts.key) {
+    if (!tokenLooksRealistic(opts.key)) {
+      throw 'The access token you have provided does not appear to be valid'
+    } else {
+      if (opts.save)
+        saveConfig(opts.key)
+
+      return opts.key
+    }
+  } else {
+    return rc.key
   }
 }
 
-function saveConfig( key ) {
+function tokenLooksRealistic(token) {
+  // Quick check if API access token looks realistic (not full validation)
+  return typeof token === 'string' && /^[\da-f]+$/.test(token)
+}
+
+function saveConfig(key) {
   let fs = require('fs'),
       file = homedir + "/." + manifest.name + "rc"
 
   try {
-    fs.writeFileSync( file, 'key = ' + key )
-    print( "Key has been saved to: " + file.yellow )
-  } catch( e ) {
-    print( e.toString().red )
+    fs.writeFileSync(file, 'key = ' + key)
+    print("Access token has been saved to: " + c.yellow(file))
+  } catch(e) {
+    print(c.red(e))
   }
 
   print()
 }
 
-//
-// Brief sanity check (not full validation)
-//
-function preValidateToken( token ) {
-  return typeof token === 'string' && token.length !== 0 && ( /^[0-9a-f]+$/ ).test( token )
-}
+async function request(method, endpoint, data) {
+  let res
 
-function getBitlyToken() {
-  return new Promise( function( resolve, reject ) {
-    if ( app.ask || ( !app.key && !preValidateToken( rc.key ) ) ) {
-      print( "Please enter your Bitly access token." )
-      print( "You can obtain your token from: " + "https://bitly.com/a/oauth_apps".yellow )
-      print()
-
-      read( { prompt: "Token: " }, function( err, key ) {
-        print()
-        if ( key !== undefined ) {
-          // Save the key if there isn't one saved already, or if --save option is passed
-          if ( app.save || !rc.key )
-            saveConfig( key )
-
-          resolve( key )
-        } else {
-          reject()
-        }
-      } )
-    } else if ( app.key ) {
-      if ( !preValidateToken( app.key ) ) {
-        reject( 'The authentication token you have provided does not appear to be valid' )
-      } else {
-        if ( app.save )
-          saveConfig( app.key )
-
-        resolve( app.key )
+  try {
+    res = await fetch(
+      'https://api-ssl.bitly.com/v4/' + endpoint, {
+        method,
+        headers: {
+          Authorization: 'Bearer ' + accessToken,
+          'Content-Type': 'application/json'
+        },
+        body: data ? JSON.stringify(data) : undefined
       }
+    )
+  } catch(e) { abort(e) }
+
+  if (res.status >= 300)
+    throw res.status
+
+  try {
+    res = await res.json()
+  } catch(e) { error(e) }
+
+  return res
+}
+
+function printItem(item) {
+  if (opts.raw) {
+    print(item)
+    return
+  }
+
+  print(
+    c.yellow(
+      normalizeBitlink((item.custom_bitlinks && item.custom_bitlinks[0]) || item.link)
+    ) + ' > ' + c.red(item.long_url)
+  )
+
+  if (opts.verbose) {
+    if (item.title && item.title !== '')
+      indent('Title:', item.title.replace(/(\r\n|\n|\r)/gm, ""))
+
+    if (item.tags && item.tags.length !== 0)
+      indent('Tags:', item.tags.join(', '))
+
+    if (item.created_at)
+      indent('Created:', new Date(item.created_at), "\n")
+  }
+}
+
+async function expandOrShorten(arg) {
+  let argUrl = makeValidURL(arg)
+
+  if (argUrl) {
+    let parts = url.parse(argUrl)
+
+    if (isBitlyDomain(parts.hostname)) {
+      await expand(parts.pathname.slice(1))
     } else {
-      resolve( rc.key )
+      await shorten(argUrl, opts.domain)
     }
-  } )
+  } else {
+    await expand(arg)
+  }
 }
 
-function makeHttps( url ) {
-  return url.replace( /^http:/, "https:" )
-}
-
-function replaceDomain( url, desiredDomain ) {
-  return url.replace( /:\/\/(.*?)\//, "://" + desiredDomain + "/" )
-}
-
-function normalizeLink( url ) {
-  return app.domain
-    ? makeHttps( replaceDomain( url, app.domain ) )
-    : makeHttps( url )
-}
-
-function shortLinkFromKey( key ) {
-  return "https://" + (app.domain || "bit.ly") + "/" + key
-}
-
-function expandOrShorten( arg ) {
-  let uri = validateURI( arg )
-
-  if ( uri ) {
-    let u = url.parse( uri )
-
-    // If the URL is a bitlink, then send a request to expand it
-    if ( domains.default.indexOf( u.hostname ) !== -1 || domains.extended.indexOf( u.hostname ) !== -1 ) {
-      expand( arg )
-    } else {
-      shorten( uri, app.domain )
+async function expand(slug) {
+  try {
+    printItem(
+      await request('GET', 'bitlinks/bit.ly/' + slug)
+    )
+  } catch(e) {
+    switch (e) {
+      case 404:
+        print(c.grey(slugToBitlink(slug)) + " is not a bitlink")
+        break
+      case 403:
+        // The bitlink belongs to someone else, so expand it a different way
+        try {
+          await expandManually(slug)
+        } catch(e) { abort(e) }
     }
-  } else
-    expand( arg )
+  }
 }
 
-function expand( shortUrl ) {
-  bitly.expand( shortUrl ).then( res => {
-    let ret = res.expand[0]
+async function expandManually(slug) {
+  let link = slugToBitlink(slug)
+  let res
 
-    if ( ret.error ) {
-      if ( ret.error === "NOT_FOUND" )
-        print( shortUrl.grey + " is not yet a bitlink" )
-      else
-        warn( "error trying to expand " + shortUrl + ": " + ret.error )
-    } else {
-      print(
-        ( ret.short_url
-          ? normalizeLink( ret.short_url )
-          : shortLinkFromKey( ret.hash )
-        ).yellow + " > " + encodeURI(ret.long_url).yellow
-      )
-    }
-  } ).catch( e => {
-    warn( e )
-  } )
+  try {
+    res = await fetch(
+      link, {
+        method: 'HEAD'
+      }
+    )
+  } catch(e) { abort(e) }
+
+  printItem({
+    link,
+    long_url: res.url
+  })
 }
 
-function shorten( longUrl, preferredDomain ) {
-  bitly.shorten( longUrl, preferredDomain ).then( res => {
-    print( makeHttps( res.url ).yellow + " (" + longUrl.grey + ")" )
-  } ).catch( e => {
-    warn( e )
-  } )
+async function shorten(longUrl, preferredDomain) {
+  let item = await request("POST", 'bitlinks', {
+    domain: preferredDomain,
+    long_url: longUrl
+  })
+
+  if (opts.raw)
+    print(item)
+  else
+    print(c.yellow(makeHttps(item.link)), "(" + c.grey(item.long_url) + ")")
 }
 
-function archive( shortUrl ) {
-  if ( !validateURI( shortUrl ) )
-    shortUrl = shortLinkFromKey( shortUrl )
+async function archive(arg) {
+  if (isURL(arg)) {
+    let parts = url.parse(arg)
 
-  bitly.bitlyRequest(
-    'user/link_edit',
-    {
-      link: shortUrl,
+    if (!isBitlyDomain(parts.hostname))
+      abort(parts.hostname + ' is not a Bitly domain')
+
+    arg = parts.pathname.substr(1)
+  }
+
+  try {
+    let item = await request('PATCH', 'bitlinks/bit.ly/' + arg, {
       edit: 'archived',
-      archived: 'true'
-    }
-  ).then( res => {
-    print( 'Archived: ' + res.link_edit.link.yellow )
-  } ).catch( e => {
-    switch( e.code ) {
-      case 400: // INVALID_ARG_LINK, which really means it exists but it's not yours
-        warn( shortUrl + " is not your bitlink" )
-        break
-      case 404: // NOT_FOUND
-        warn( shortUrl + " is not yet a bitlink" )
-        break
-      default:
-        abort( e )
-    }
-  } )
-}
+      archived: true
+    })
 
-function history( offset ) {
-  offset = offset || 0
-  let count = Math.min( app.count, BITLY_MAX_HISTORY_CHUNK )
-
-  if ( count !== 0 ) {
-    app.count -= count
-
-    bitly.bitlyRequest(
-      'user/link_history',
-      {
-        offset,
-        limit: count
-      }
-    ).then( res => {
-      printHistory( res.link_history )
-
-      if ( offset + count < res.result_count )
-        history( offset + count )
-    } ).catch( e => {
-      abort( e )
-    } )
+    if (opts.raw)
+      print(item)
+    else
+      print('Archived: ' + c.yellow(makeHttps(item.link)))
+  } catch(e) {
+    if (e === 403)
+      print(c.gray(arg) + " is not your bitlink")
+    else if (e === 404)
+      print(c.gray(arg) + " is not a bitlink")
   }
 }
 
-function printHistory( link_history ) {
-  for ( let item of link_history ) {
-    // Print bitlink followed by original URL
-    print(
-      ( (item.keyword_link === undefined)
-        ? normalizeLink( item.link ).yellow         // item.link is a default named bitlink, e.g. https://j.mp/2p8n9WY
-        : normalizeLink( item.keyword_link ).yellow // item.keyword_link is a customized bitlink, like: https://j.mp/a
-      ) +' > ' + encodeURI(item.long_url).red )
+async function history(groupId, page) {
+  const maxItems = 100
 
-    // Print additional details (if "--verbose")
-    if ( app.verbose ) {
-      const INDENT = '     '
+  let num = Math.min(opts.count, maxItems)
+  opts.count -= num
 
-      if ( item.title !== "" )
-        print( INDENT + item.title.replace( /(\r\n|\n|\r)/gm, "") )
-      if ( item.tags.length !== 0 )
-        print( INDENT + 'Tags: '.red + item.tags.join(', ').yellow )
+  let results
 
-      print( INDENT + 'Created: '.red + new Date(item.created_at * 1000) + "\n" )
+  if (!page) {
+    let user = await request('GET', 'user')
+
+    groupId = user.default_group_guid
+    page = 1
+
+    results = await request('GET', `groups/${groupId}/bitlinks?size=${num}`)
+  } else
+    results = await request('GET', `groups/${groupId}/bitlinks?page=${page}&size=${num}`)
+
+  results.links.forEach(
+    item => {
+      printItem(item)
     }
+  )
+
+  if (opts.count > 0 && results.pagination.next !== '')
+    history(groupId, page + 1)
+}
+
+function isBitlyDomain(domain) {
+  return domains.default.includes(domain) || domains.extended.includes(domain)
+}
+
+function slugToBitlink(slug) {
+  return "https://" + (opts.domain || "bit.ly") + "/" + slug
+}
+
+function normalizeBitlink(url) {
+  return opts.domain
+    ? makeHttps(replaceDomain(url, opts.domain))
+    : makeHttps(url)
+}
+
+function replaceDomain(url, desiredDomain) {
+  return url.replace(/:\/\/(.*?)\//, "://" + desiredDomain + "/")
+}
+
+function makeHttps(url) {
+  return url.replace(/^http:/, "https:")
+}
+
+function makeValidURL(s) {
+  if (isURL(s)) {
+    return s
+  } else {
+    s = 'https://' + s
+
+    return isURL(s) ? s : null
   }
+}
+
+function isURL(s) {
+  // Small but effective URI validator by Diego Perini:
+  //   https://gist.github.com/dperini/729294
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(s)
 }
